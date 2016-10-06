640fc7918054b20f1985947ea885ced0f64f62a4
MacPro <enricomaria.deangelis@unina.it>
2016-10-06 15:03:01 +0200

    Important modification to variables_mod.f90
    Added one more field to the uvwp variable's type; in this way it contains lower and upper bounds, along the three dimensions, considering the boundary (if touched), the overlap (even if touches the border, thus avoiding the problem of zero dimension for MPI_type_create_subarray), and both (this time the overlap is not considered if the process touches the boundary, since these bounds are used to allocate uvwp( )%values.

--- variables_mod.f90
+++ variables_mod.f90
@@ -30,15 +30,15 @@ MODULE variables
     ! Type used to contain the fluiddynamic variables.
     TYPE block
         REAL,    DIMENSION(:,:,:), ALLOCATABLE :: values
-        INTEGER, DIMENSION(:,:),   ALLOCATABLE :: b    ! bounds not considering, borders  nor overlap
+        INTEGER, DIMENSION(:,:),   ALLOCATABLE :: b    ! bounds not considering  borders  nor overlap
         INTEGER, DIMENSION(:,:),   ALLOCATABLE :: b_bc ! bounds considering      borders, not overlap
-        INTEGER, DIMENSION(:,:),   ALLOCATABLE :: b_ol ! bounds considering      borders  and overlap
+        INTEGER, DIMENSION(:,:),   ALLOCATABLE :: b_ol ! bounds not considering  borders, but overlap
+        INTEGER, DIMENSION(:,:),   ALLOCATABLE :: b_bo ! bounds considering both borders  and overlap
     END TYPE block
     TYPE(block), DIMENSION(n_flow_variables), TARGET :: uvwp ! TO DO: meglio 0:n_flow_variables-1, associando 0 alla pressione e 1,... alle componenti di velocità
 
     ! MPI types to manage communications
     INTEGER, DIMENSION(:,:), ALLOCATABLE :: MPI_flats_m, MPI_flats_p
-    INTEGER :: ic, id
 
 CONTAINS
 
@@ -56,13 +56,13 @@ CONTAINS
 
         IMPLICIT NONE
 
-        INTEGER :: errorcode, ierr, ic, id, ld1, ud1, ld2, ud2, i
+        INTEGER :: errorcode, ierr, ic, id, ld1, ud1, ld2, ud2, i, id2
         INTEGER, DIMENSION(ndims) :: array_of_sizes, array_of_subsizes_m, array_of_subsizes_p, array_of_starts
-        INTEGER(KIND=MPI_ADDRESS_KIND) :: address1, address2, address3, extent
+        ! INTEGER(KIND=MPI_ADDRESS_KIND) :: address1, address2, address3, extent
 
         ALLOCATE(MPI_flats_m(ndims,ndims), MPI_flats_p(ndims,ndims))
         DO i = 1, n_flow_variables
-            ALLOCATE(uvwp(i)%b(ndims, 2), uvwp(i)%b_bc(ndims, 2), uvwp(i)%b_ol(ndims, 2))
+            ALLOCATE(uvwp(i)%b(ndims, 2), uvwp(i)%b_bc(ndims, 2), uvwp(i)%b_ol(ndims, 2), uvwp(i)%b_bo(ndims, 2))
         END DO
 
         select_the_number_of_dimensions: SELECT CASE (ndims)
@@ -89,73 +89,67 @@ CONTAINS
                     for_each_direction_compute_size: DO id = 1, ndims
 
                         ! bounds of the velocity array not considering boundaries nor overlap
-                        uvwp(ic)%b(id,:) = [(1 + KronDelta(id,ic))*log2int(idm(id) == MPI_PROC_NULL) + &
-                                          & log2int(idm(id) /= MPI_PROC_NULL), N(id)]
+                        uvwp(ic)%b(id,:) = [1 + KronDelta(id,ic)*log2int(idm(id) == MPI_PROC_NULL), N(id)]
 
                         ! retrieve the number of diagonals of the B matrix for
                         ! interpolation and second derivative along the current
-                        ! direction for the current component
+                        ! direction for the current component (take the max between the two)
                         ld1 = MAX(cmp(id,0,1)%B%lid, cmp(id,2,1)%B%lid) ! number of lower diagonals from centers (to faces or to centers)
                         ud1 = MAX(cmp(id,0,1)%B%uid, cmp(id,2,1)%B%uid) ! number of upper diagonals from centers (to faces or to centers)
                         ld2 = MAX(cmp(id,0,2)%B%lid, cmp(id,2,2)%B%lid) ! number of lower diagonals from faces   (to faces or to centers)
                         ud2 = MAX(cmp(id,0,2)%B%uid, cmp(id,2,2)%B%uid) ! number of upper diagonals from faces   (to faces or to centers)
 
-                        ! modify the lower bound of velocity array considering the boundaries...
+                        ! TODO: the following (till END DO) can be simplified a lot (the modifications
+                        ! to obtain b_bc and b_ol are mutually exclusive!)
+                        ! modify the lower bound of velocity array considering the boundaries
                         uvwp(ic)%b_bc(id,1) = uvwp(ic)%b(id,1) &
                                   & - log2int(idm(id) == MPI_PROC_NULL) ! minus 1 if touches the minus border along the current direction
-                        ! ... and the overlap
-                        uvwp(ic)%b_ol(id,1) = uvwp(ic)%b_bc(id,1) &
+                        ! modify the lower bound of velocity array considering the overlap (regardless the possible non periodicity)
+                        uvwp(ic)%b_ol(id,1) = uvwp(ic)%b(id,1) &
+                                  & - ld1*(1 - KronDelta(id,ic)) &! minus the number of lower diagonals from centers for the non-ic-th direction 
+                                  & - ld2*KronDelta(id,ic)        ! minus the number of lower diagonals from faces   for the     ic-th direction
+                        ! modify the lower bound of velocity array considering the boundaries and the overlap
+                        uvwp(ic)%b_bo(id,1) = uvwp(ic)%b(id,1) &
+                                  & - log2int(idm(id) == MPI_PROC_NULL) & ! minus 1 if touches the minus border along the current direction
                                   & - log2int(idm(id) /= MPI_PROC_NULL)*ld1*(1 - KronDelta(id,ic)) &! minus the number of lower diagonals from centers for the non-ic-th direction 
                                   & - log2int(idm(id) /= MPI_PROC_NULL)*ld2*KronDelta(id,ic)        ! minus the number of lower diagonals from faces   for the     ic-th direction
 
-                        ! modify the upper bound of velocity array considering the boundaries...
+                        ! modify the lower bound of velocity array considering the boundaries
                         uvwp(ic)%b_bc(id,2) = uvwp(ic)%b(id,2) &
                                   & + log2int(idp(id) == MPI_PROC_NULL) ! plus 1 if touches the plus border along the current direction
-                        ! ... and the overlap
-                        uvwp(ic)%b_ol(id,2) = uvwp(ic)%b_bc(id,2) &
+                        ! modify the lower bound of velocity array considering the overlap (regardless the possible non periodicity)
+                        uvwp(ic)%b_ol(id,2) = uvwp(ic)%b(id,2) &
+                                  & + ud1*(1 - KronDelta(id,ic)) &! plus the number of upper diagonals from centers for the non-ic-th direction
+                                  & + ud2*KronDelta(id,ic)        ! plus the number of upper diagonals from faces   for the     ic-th direction
+                        ! modify the lower bound of velocity array considering the boundaries and the overlap
+                        uvwp(ic)%b_bo(id,2) = uvwp(ic)%b(id,2) &
+                                  & + log2int(idp(id) == MPI_PROC_NULL) & ! plus 1 if touches the plus border along the current direction
                                   & + log2int(idp(id) /= MPI_PROC_NULL)*ud1*(1 - KronDelta(id,ic)) &! plus the number of upper diagonals from centers for the non-ic-th direction
                                   & + log2int(idp(id) /= MPI_PROC_NULL)*ud2*KronDelta(id,ic)        ! plus the number of upper diagonals from faces   for the     ic-th direction
 
                     END DO for_each_direction_compute_size
 
                     ! allocate velocity component array
-                    ALLOCATE(uvwp(ic)%values(uvwp(ic)%b_ol(1,1):uvwp(ic)%b_ol(1,2), &
-                                           & uvwp(ic)%b_ol(2,1):uvwp(ic)%b_ol(2,2), &
-                                           & uvwp(ic)%b_ol(3,1):uvwp(ic)%b_ol(3,2)))
+                    ALLOCATE(uvwp(ic)%values(uvwp(ic)%b_bo(1,1):uvwp(ic)%b_bo(1,2), &
+                                           & uvwp(ic)%b_bo(2,1):uvwp(ic)%b_bo(2,2), &
+                                           & uvwp(ic)%b_bo(3,1):uvwp(ic)%b_bo(3,2)))
 
-                    ! shape of the velocity component array with ghost cells
+                    ! shape of the velocity component array with ghost cells and boundaries
                     array_of_sizes = SHAPE(uvwp(ic)%values)
 
                     for_each_direction_compute_subsize: DO id = 1, ndims
 
                         ! shape of the velocity component sub-array
-                        array_of_subsizes_m = [KronDelta(id,1)*(1 - uvwp(ic)%b_ol(1,1)) + &
-                                                & (1 - KronDelta(id,1))*(N(1)*KronDelta(id,ic) + &
-                                                & (N(1) - log2int(idm(ic) == MPI_PROC_NULL))*(1 - KronDelta(id,ic))), &
-                                             & KronDelta(id,2)*(1 - uvwp(ic)%b_ol(2,1)) + &
-                                                & (1 - KronDelta(id,2))*(N(2)*KronDelta(id,ic) + &
-                                                & (N(2) - log2int(idm(ic) == MPI_PROC_NULL))*(1 - KronDelta(id,ic))), &
-                                             & KronDelta(id,3)*(1 - uvwp(ic)%b_ol(3,1)) + &
-                                                & (1 - KronDelta(id,3))*(N(3)*KronDelta(id,ic) + &
-                                                & (N(3) - log2int(idm(ic) == MPI_PROC_NULL))*(1 - KronDelta(id,ic)))]
-
-                        array_of_subsizes_p = [KronDelta(id,1)*(uvwp(ic)%b_ol(1,2) - N(1)) + &
-                                                & (1 - KronDelta(id,1))*(N(1)*KronDelta(id,ic) + &
-                                                & (N(1) - log2int(idm(ic) == MPI_PROC_NULL))*(1 - KronDelta(id,ic))), &
-                                             & KronDelta(id,2)*(uvwp(ic)%b_ol(2,2) - N(2)) + &
-                                                & (1 - KronDelta(id,2))*(N(2)*KronDelta(id,ic) + &
-                                                & (N(2) - log2int(idm(ic) == MPI_PROC_NULL))*(1 - KronDelta(id,ic))), &
-                                             & KronDelta(id,3)*(uvwp(ic)%b_ol(3,2) - N(3)) + &
-                                                & (1 - KronDelta(id,3))*(N(3)*KronDelta(id,ic) + &
-                                                & (N(3) - log2int(idm(ic) == MPI_PROC_NULL))*(1 - KronDelta(id,ic)))]
-
-                        DO i = 1, ndims
-                            IF (array_of_subsizes_m(i) == 0) THEN
-                                array_of_subsizes_m(i) = 1
-                            END IF
-                            IF (array_of_subsizes_p(i) == 0) THEN
-                                array_of_subsizes_p(i) = 1
-                            END IF
+                        ! TODO: in a far future I could combine array_of_subsizes_m and array_of_subsizes_p
+                        ! into one 2D array so that the following DO would contain just one assignation
+                        ! in another DO = 1,2
+                        DO id2 = 1, ndims
+                            array_of_subsizes_m(id2) = KronDelta(id,id2)*(1 - uvwp(ic)%b_ol(id2,1)) + &
+                                                & (1 - KronDelta(id,id2))*(N(id2)*KronDelta(id,ic) + &
+                                                & (N(id2) - log2int(idm(ic) == MPI_PROC_NULL))*(1 - KronDelta(id,ic)))
+                            array_of_subsizes_p(id2) = KronDelta(id,id2)*(1 - uvwp(ic)%b_ol(id2,1)) + &
+                                                & (1 - KronDelta(id,id2))*(N(id2)*KronDelta(id,ic) + &
+                                                & (N(id2) - log2int(idp(ic) == MPI_PROC_NULL))*(1 - KronDelta(id,ic)))
                         END DO
 
                         array_of_starts = [0, 0, 0]
@@ -171,6 +165,9 @@ CONTAINS
                                                     & MPI_ORDER_FORTRAN, &
                                                     & MPI_DOUBLE_PRECISION, MPI_flats_p(ic,id), &
                                                     & ierr)
+
+                        CALL MPI_TYPE_COMMIT(MPI_flats_m(ic,id),ierr)
+                        CALL MPI_TYPE_COMMIT(MPI_flats_p(ic,id),ierr)
 
                     END DO for_each_direction_compute_subsize
 
